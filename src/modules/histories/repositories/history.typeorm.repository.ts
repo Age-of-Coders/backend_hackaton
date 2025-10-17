ed257aae75b21239c1ac2b7411769a0aa4b5280a
Tobías Vega <tobiasvega1210@gmail.com>
2025-10-17 03:44:42 -0300

    fix: :bug: Se devuelve el id

--- src/modules/histories/repositories/history.typeorm.repository.ts
+++ src/modules/histories/repositories/history.typeorm.repository.ts
@@ -16,9 +16,9 @@ export class HistoryTypeOrmRepository {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  async findAllSimple(): Promise<{ created_at: Date; texto_historia: string; likes: number }[]> {
+  async findAllSimple(): Promise<{ id: string; created_at: Date; texto_historia: string; likes: number }[]> {
     return await this.historyRepository.find({
-      select: ['created_at', 'texto_historia', 'likes'],
+      select: ['id', 'created_at', 'texto_historia', 'likes'],
       order: { created_at: 'DESC' }
     });
   }
